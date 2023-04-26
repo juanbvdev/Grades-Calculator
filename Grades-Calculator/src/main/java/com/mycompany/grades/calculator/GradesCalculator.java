85e51cb7297e9dab2c6e78bd5d0f56cde49b6cef
devbyjuan <juansurfvieira123@gmail.com>
2023-04-25 21:43:55 -0300

    Adicionado recomendação de nota em relação à divisão letiva.

--- Grades-Calculator/src/main/java/com/mycompany/grades/calculator/GradesCalculator.java
+++ Grades-Calculator/src/main/java/com/mycompany/grades/calculator/GradesCalculator.java
@@ -14,50 +14,83 @@ public class GradesCalculator {
 
     public static void bimestre(float n1, float n2, float n3, float n4){
         
-        float med, medschool, max, bi = 0;
-        int op = 0;
+        float nota, medschool, max, bi = 0;
+        int op, nulo = 0;
         System.out.print("Insira a nota máxima da escola: ");
         max = f.nextFloat();
         System.out.print("Insira a média da escola: ");
         medschool = f.nextFloat();
         bi = max / 4;
         
+        if(n1 < 0){
+            nulo++;
+            n1 = 0;
+        }
+        if(n2 < 0){
+            nulo++;
+            n2 = 0;
+        }
+        if(n3 < 0){
+            nulo++;
+            n3 = 0;
+        }
+        if(n4 < 0){
+            nulo++;
+            n4 = 0;
+        }
+        
         System.out.println("1 - Calcular quanto falta\n2 - ");
         System.out.print("Escolha");
         op = i.nextInt();
         
         switch(op){
             case 1 -> {
-                med = n1 + n2 + n3 + n4;
+                nota = n1 + n2 + n3 + n4;
                 
-                System.out.println("Nota: " + med);
+                System.out.println("Nota: " + nota);
                 
-                if(med > medschool){
+                if(nota > medschool){
                     System.out.println("Aprovado!");
                 }else{
-                    if(medschool - med <= bi){
-                        System.out.println("Faltam " + (medschool - med) + " pontos!");
+                    if(medschool - nota <= bi || nulo >= 2){
+                        System.out.println("Faltam " + (medschool - nota) + " pontos!");
+                        System.out.print("Você pode tirar " + ((medschool - nota) / nulo));
+                        System.out.println(" em cada um dos próximos " + nulo + " bimestres.");
                     }else{
-                        System.out.print("Reprovado! faltou " + (medschool - med) + " pontos");
+                        System.out.print("Reprovado! faltou " + (medschool - nota) + " pontos");
                         System.out.println(" para atingir a média.");
                         System.out.println("Só é possível tirar " + bi + " pontos por bimestre...");
                     }
                     
                 }
             }
+
         }
         
     }
     
     public static void trimestre(float n1, float n2, float n3){
         
-        float med, medschool, max, tri = 0;
-        int op = 0;
+        float nota, medschool, max, tri = 0;
+        int op, nulo = 0;
         System.out.print("Insira a nota máxima da escola: ");
         max = f.nextFloat();
         System.out.print("Insira a média da escola: ");
         medschool = f.nextFloat();
         tri = max / 3;
+        
+        if(n1 < 0){
+            nulo++;
+            n1 = 0;
+        }
+        if(n2 < 0){
+            nulo++;
+            n2 = 0;
+        }
+        if(n3 < 0){
+            nulo++;
+            n3 = 0;
+        }
         
         System.out.println("1 - Calcular quanto falta\n2 - ");
         System.out.print("Escolha");
@@ -65,30 +98,33 @@ public class GradesCalculator {
         
         switch(op){
             case 1 -> {
-                med = n1 + n2 + n3;
+                nota = n1 + n2 + n3;
                 
-                System.out.println("Nota: " + med);
+                System.out.println("Nota: " + nota);
                 
-                if(med > medschool){
+                if(nota > medschool){
                     System.out.println("Aprovado!");
                 }else{
-                    if(medschool - med <= tri){
-                        System.out.println("Faltam " + (medschool - med) + " pontos!");
+                    if(medschool - nota <= tri || nulo >= 1){
+                        System.out.println("Faltam " + (medschool - nota) + " pontos!");
+                        System.out.print("Você pode tirar " + ((medschool - nota) / nulo));
+                        System.out.println(" em cada um dos próximos " + nulo + " bimestres.");
                     }else{
-                        System.out.print("Reprovado! faltou " + (medschool - med) + " pontos");
+                        System.out.print("Reprovado! faltou " + (medschool - nota) + " pontos");
                         System.out.println(" para atingir a média.");
                         System.out.println("Só é possível tirar " + tri + " pontos por trimestre...");
                     }
                     
                 }
             }
+
         }
         
     }
     
     public static void semestre(float n1, float n2){
         
-        float med, medschool, max, sem = 0;
+        float nota, medschool, max, sem = 0;
         int op = 0;
         System.out.print("Insira a nota máxima da escola: ");
         max = f.nextFloat();
@@ -102,23 +138,24 @@ public class GradesCalculator {
         
         switch(op){
             case 1 -> {
-                med = n1 + n2;
+                nota = n1 + n2;
                 
-                System.out.println("Nota: " + med);
+                System.out.println("Nota: " + nota);
                 
-                if(med > medschool){
+                if(nota > medschool){
                     System.out.println("Aprovado!");
                 }else{
-                    if(medschool - med <= sem){
-                        System.out.println("Faltam " + (medschool - med) + " pontos!");
+                    if(medschool - nota <= sem){
+                        System.out.println("Faltam " + (medschool - nota) + " pontos!");
                     }else{
-                        System.out.print("Reprovado! faltou " + (medschool - med) + " pontos");
+                        System.out.print("Reprovado! faltou " + (medschool - nota) + " pontos");
                         System.out.println(" para atingir a média.");
                         System.out.println("Só é possível tirar " + sem + " pontos por trimestre...");
                     }
                     
                 }
             }
+
         }
         
     }
@@ -138,6 +175,9 @@ public class GradesCalculator {
 //------------------------------------------------------------------------------
         
         System.out.println("Grades Calculator\n\n");
+        
+        System.out.println("Se o bimestre/trimestre/semestre ainda não ocorreu...");
+        System.out.println(" insira um valor negativo na nota.\n\n");
         
         System.out.print("1 - bimestre\n2 - trimestre\n3 - semestre\nEscolha: ");
         op = i.nextInt();
